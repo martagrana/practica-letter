07a869ae201729e30c1ad62ecf8a2a24a179c219
Marta Grana <marta.grana.fdez@gmail.com>
2023-11-09 12:47:06 +0100

    crear practica letter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'letter';
+  ClincName: string = 'Clinic Name';
+  ProviderName: string = 'Provider Name';
+  ProviderAdress: string = 'Provider Adress';
+  Date: string = 'Date';
+  DOB: string = 'DOB';
+  Testingdate: string = '10/23/2018';
+  phonenumber: string = '981263224';
+  email: string = 'pruebacarta@gmail.com';
+  CHWName: string = 'CHW Name';
+  AgencyName: string = 'Agency Name';
+
+  Fasting: string = 'Yes';
+  Bloodpressure: string = '149/76';
+  Totalcholesterol: string = '235';
+  Glucose: string = '106';
+  HDL: string = '60';
+  HbA1c: string = 'NA';
+  LDL: string = '150';
+  Bodymassindex: string = '29';
+  Triglycerides: string = '130';
+  Smokingstatus: string = 'Smoking';
+  NonHDL: string = '175';
+  Stressscore: string = 'Mild (10)';
+  Anxietyscore: string = 'Mild (8)';
 }
